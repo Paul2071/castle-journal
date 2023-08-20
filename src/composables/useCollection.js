627677899d69e886e58489ea963db4b2b9308bfa
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-08-20 15:35:47 +0100

    added basic form prompting user for email and password

--- src/composables/useCollection.js
+++ src/composables/useCollection.js
@@ -14,7 +14,7 @@ const useCollection = (collection) => {
     }
     catch(err) {
       console.log(err.message)
-      error.value = 'could not send the message'
+      error.value = 'could not load the post'
     }
   }
 
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,17 @@
+<template>
+   <form>
+    <h3>Login form</h3>
+    <input type="email" placeholder="Enter email..." v-model="email">
+    <input type="password" placeholder="Enter password..." v-model="password">
+    <button>Log in</button>
+   </form>
+</template>
+
+<script >
+export default {
+
+}
+</script>
+
+<style >
+</style>
