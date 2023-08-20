323387b20035163e3637cbb1f0ca96f8ad66d504
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-08-20 15:54:53 +0100

    added a user to firebase console, can login and display an error if there is one. Woohoo!

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,14 +1,37 @@
 <template>
-   <form>
+   <form @submit.prevent="handleSubmit">
     <h3>Login form</h3>
     <input type="email" placeholder="Enter email..." v-model="email">
     <input type="password" placeholder="Enter password..." v-model="password">
+    <div v-if="error" class="error"> {{ error }}</div>
     <button>Log in</button>
-   </form>
+</form>
 </template>
 
 <script >
-export default {
+
+import useLogin from '../../composables/useLogin'
+import { ref } from 'vue'
+
+export default {    
+    setup () {
+
+        const { error, login } = useLogin()
+
+        const email = ref('')
+        const password = ref('')
+
+        const handleSubmit = async () => {
+           
+            const res = await login(email.value, password.value)
+            if (!error.value) {
+                console.log('login successful')
+             }
+        }
+
+        return { email, password, handleSubmit, error }
+
+    }
 
 }
 </script>
