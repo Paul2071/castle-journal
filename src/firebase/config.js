2ab1393937215ddade6ceec3315872ee0376236e
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-08-27 23:18:21 +0100

    composable created that uploads image to firebase storage. storage initialised in config file, exported, imported in composable, syntax used in function, function exported ready to be used elsewhere

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 
 const firebaseConfig = {
@@ -18,8 +19,9 @@ const firebaseConfig = {
   // init firestore service
   const projectFirestore = firebase.firestore()
   const projectAuth = firebase.auth()
+  const projectStorage  = firebase.storage()
 
   //timestamp
   const timestamp = firebase.firestore.FieldValue.serverTimestamp
 
-  export { projectFirestore, projectAuth, timestamp }
+  export { projectFirestore, projectAuth, projectStorage, timestamp }
