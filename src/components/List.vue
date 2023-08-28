ecf7943e79519df6f68f59e35f430c62106774bf
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-08-29 00:51:34 +0100

    added list component that gets data from firebase and slaps it on page. scoped styling makes it look okay. currently displaying the image uploaded, the title and created by. Will probably change them later

---
+++ src/components/List.vue
@@ -0,0 +1,56 @@
+<template>
+  <div v-for="journal in castlejournal" :key="journal.id">
+    <div class="single">
+      <div class="thumbnail">
+        <img :src="journal.castleImage" />
+      </div>
+      <div class="info">
+        <h3> {{ journal.title }}</h3>
+        <p> Created by {{  journal.userName }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["castlejournal"],
+};
+</script>
+<style scoped >
+
+.single {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    border-radius: 10px;
+    background: white;
+    margin: 16px 0;
+    transition: all ease 0.2s;
+}
+.single:hover {
+    box-shadow: 1px 2px 3px rgba(50, 50, 50, 50,);
+    border-color: black;
+    transform: scale(1.02);
+    border-style: ridge;
+    transition: all ease 0.2s;
+    cursor: pointer;
+    
+}
+.thumbnail {
+    max-width: 100px;
+    max-height: 100px;
+    overflow: hidden;
+    border-radius: 10px;
+}
+img {
+    max-width: 150%;
+    max-height: 150%;
+    display: block;
+}
+.info {
+    margin: 0 30px;
+}
+
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,24 +2,24 @@
   <div class="home">
     <div v-if="error" class="error">Could not get your posts...</div>
     <div v-if="documents" >
-      <di v-for="doc in documents" :key="doc.id"> {{ doc.title }}</di>
-    </div>
+      <List :castlejournal="documents"/>
+      <!-- <div v-for="doc in documents" :key="doc.id"> {{ doc.title }}</div> -->
+    </div> 
   </div>
   
 </template>
 
 <script>
+import List from '@/components/List.vue';
 import getCollections from '../composables/getCollections';
 
 export default {
-  name: 'HomeView',
-  setup() {
-  const { error, documents} =  getCollections('castlejournal')
-
-
-  
-
-  return { documents, error }
-  }
+    name: 'HomeView',
+    components: { List },
+    setup() {
+        const { error, documents } = getCollections('castlejournal');
+        return { documents, error };
+    },
+    components: { List }
 }
 </script>
